9ae4e8e2f52f1aa8fddfdd4b3c9a3cf89760ba3b
Jeremias Celidon <jec222@txstate.edu>
2020-11-03 00:53:41 +0000

    testing

--- dlist.h
+++ dlist.h
@@ -76,13 +76,114 @@ class Dlist {
 /***************************************
  *ADD  Member function implementations here
  ***************************************/
+ /*PSEUDOCODE
+ IsEmpty()
+  if null first is not null return 1
+  else return 0 
+
+InsertFront(const T &o)
+  create a pointer to a new node 
+  set new pointer data to o
+
+  if first equals nullptr
+    set last and first to temp
+
+  else
+    make first prev equal to temp
+    make temp next  equal to first 
+    make first equal to temp
+
+InsertBack(const T &o)
+  create a re node that will be added to the back
+  set temp node data to o from the parameter 
+
+  if last is equal to nullptr
+    set first and last to temp 
+
+  else
+    make temp new last 
+    make temp prev equal to last 
+    make last equal to temp
+
+RemoveFront()
+  if list is not empty
+    set a temp node equal to first
+    set value being delited to delValue of data type T
+
+  if first and last are the same 
+     make last and first equal to nullptr
+
+  else
+    set first to its next node value 
+    set first prev equal to nullptr
+
+  delete temp;
+  return delValue;
+
+RemoveBack()
+ if list is not empty
+    set a temp node equal to last 
+    set a delValue to be equal the last o value
+    
+    if first and last are the same 
+      make last and first equal to nullptr
+    
+    else
+      set last equal to last prev value
+      set last next equal to nullptr
+    
+    delete temp; 
+    return delValue;
+
+
+Dlist()
+  call MakeEmpty function to make an empty list
+
+Dlist(const Dlist &l)
+  call CopyAll to set equal to l
+ 
+operator= (const Dlist &l)
+
+  if the curent list equal to l
+    return the the curent list 
+
+  else
+    call RemoveAll and CopyAll and return curent list
+
+~Dlist()
+  call RemoveAll method to delete all nodes in the list
+MakeEmpty()
+  set first and last equal to nullptr
+
+RemoveAll()
+
+  make temp equal to first 
+
+  while not the end of the list 
+    set first to the next node in the list 
+    delete temp
+    make temp equal to first 
+
+  set first equal to nullptr
+    
+CopyAll(const Dlist &l)
+
+  if list l is empy
+   make curent list empty
+
+  else
+    create new node called temp set equal to fist none from l
+
+    while the list is not empty 
+      set nodes from l equal to l by calling InsertBack
+      set temp equal to temp next
+ */
 template <typename T>
 bool Dlist<T>::IsEmpty() const{
-   return (first == nullptr && last == nullptr);
+   return !first?1:0;
 }
 template <typename T>
 void Dlist<T>::InsertFront(const T &o){
-   // if list is empty
    node* temp = new node;
    temp -> o = o;
 
@@ -151,7 +252,7 @@ T Dlist<T>::RemoveBack(){
 }
 template <typename T>
 Dlist<T>::Dlist(){
-  first = last = nullptr;
+  MakeEmpty();
 }       
 template <typename T>                 
 Dlist<T>::Dlist(const Dlist &l){
@@ -159,9 +260,13 @@ Dlist<T>::Dlist(const Dlist &l){
 } 
 template <typename T>     
 Dlist<T>& Dlist<T>::operator= (const Dlist &l){      
-//Dlist&<T>::Dlist operator=(const Dlist &l){
+  
+  if (this == &l) 
+    return *this;
+
   RemoveAll();
   CopyAll(l); 
+  return *this;
 } 
 template <typename T>        
 Dlist<T>::~Dlist(){
@@ -184,11 +289,16 @@ void Dlist<T>::RemoveAll(){
 }
 template <typename T>
 void Dlist<T>::CopyAll(const Dlist &l){
-  node* temp = l.first -> next;
 
-  while(temp != nullptr){
-    InsertBack(temp -> o);
-    temp = temp -> next;
+  if (l.first == nullptr) {
+    first = last = nullptr;
+  } 
+  else {
+    node* temp = l.first;
+    while (temp) {
+      InsertBack(temp->o);
+      temp = temp->next;
+    }
   }
 }
 
